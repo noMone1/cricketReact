3fa443944629c6d5cff2025e01780e7cdab8552a
anupamtechsimians <anupams@techsimians.com>
2023-07-06 19:14:08 +0530

    added carausal

--- src/components/Carausal/Carausal.js
+++ src/components/Carausal/Carausal.js
@@ -1,29 +1,47 @@
-import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
-function Carousel() {
+function CarouselFadeExample() {
   return (
-    <div id="carouselExampleControls" className="carousel slide" data-ride="carousel" style={{width:"100%"}}>
-      <div className="carousel-inner">
-        <div className="carousel-item active">
-          <img className="d-block w-100" src="https://picsum.photos/200" alt="First slide" />
-        </div>
-        <div className="carousel-item">
-          <img className="d-block w-100" src="https://picsum.photos/200" alt="Second slide" />
-        </div>
-        <div className="carousel-item">
-          <img className="d-block w-100" src="https://picsum.photos/200" alt="Third slide" />
-        </div>
-      </div>
-      <a className="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
-        <span className="carousel-control-prev-icon" aria-hidden="true"></span>
-        <span className="sr-only">Previous</span>
-      </a>
-      <a className="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
-        <span className="carousel-control-next-icon" aria-hidden="true"></span>
-        <span className="sr-only">Next</span>
-      </a>
-    </div>
+    <Carousel fade>
+      <Carousel.Item>
+        <img height="150"
+          className="d-block w-100"
+          src="https://picsum.photos/200/300"
+          alt="First slide"
+        />
+        <Carousel.Caption>
+          <h3>First slide label</h3>
+          <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img height="150"
+          className="d-block w-100"
+          src="https://picsum.photos/200/300"
+          alt="First slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Second slide label</h3>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+        </Carousel.Caption>
+      </Carousel.Item>
+      <Carousel.Item>
+      <img height="150"
+          className="d-block w-100"
+          src="https://picsum.photos/200/300"
+          alt="First slide"
+        />
+
+        <Carousel.Caption>
+          <h3>Third slide label</h3>
+          <p>
+            Praesent commodo cursus magna, vel scelerisque nisl consectetur.
+          </p>
+        </Carousel.Caption>
+      </Carousel.Item>
+    </Carousel>
   );
 }
 
-export default Carousel;
+export default CarouselFadeExample;
--- src/components/MatchList.js
+++ src/components/MatchList.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import Carousel from './Carausal/Carausal';
 import Listing from './Listing/Listing';
+
 const MatchList = () => {
   return (
-    <>
-<Listing/>
- 
-    </>
+    <div>
+      <div className="p-0">
+        <Carousel />
+      </div>
+      <div>
+        <Listing />
+      </div>
+    </div>
   );
 };
 
